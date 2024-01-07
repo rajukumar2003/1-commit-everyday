0d45d7c16b62e4d4dc15f13f9a8178411834d5b5
Raju Kumar <111283679+rajukumar2003@users.noreply.github.com>
2024-01-07 17:44:59 +0530

    Update 1-promisify-setTimeout.js

--- Assignments/Week-2/Hard/1-promisify-setTimeout.js
+++ Assignments/Week-2/Hard/1-promisify-setTimeout.js
@@ -1,7 +1,3 @@
-/*
-    Write a function that returns a promise that resolves after n seconds have passed,
-     where n is passed as an argument to the function.
-*/
 function mypromise() {
     var p = new Promise(function (resolve) {
 
