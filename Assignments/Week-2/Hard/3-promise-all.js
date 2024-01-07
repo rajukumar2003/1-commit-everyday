bbaa30a55b959666177a4916992e55f536e186c2
Raju Kumar <111283679+rajukumar2003@users.noreply.github.com>
2024-01-07 17:44:36 +0530

    Update 3-promise-all.js

--- Assignments/Week-2/Hard/3-promise-all.js
+++ Assignments/Week-2/Hard/3-promise-all.js
@@ -1,8 +1,3 @@
-/*
- * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
- * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
- * Print how long it took for all 3 promises to resolve.
- */
 
 function waitOneSecond() {
     const p1 = new Promise(function (resolve) {
