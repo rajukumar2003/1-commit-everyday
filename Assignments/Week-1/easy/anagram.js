9c133ca5385548670bdfbc45f2c8904a89d458e2
Raju Kumar <111283679+rajukumar2003@users.noreply.github.com>
2024-01-11 12:24:00 +0530

    Create anagram.js

---
+++ Assignments/Week-1/easy/anagram.js
@@ -0,0 +1,34 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let count = 0;
+  if (str1.length == str2.length) {
+
+    for (let i = 0; i < str1.length; i++) {
+      for (let j = 0; j < str2.length; j++) {
+        if (str1[i] == str2[j]) {
+          // console.log(count);
+          count++;
+          break;
+        }
+      }
+    }
+    // console.log(count);
+    if (count == str1.length) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
+}
+
+module.exports = isAnagram;
